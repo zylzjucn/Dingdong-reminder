602bcfabceae8dff8dca4761954eca4aeabc27c9
Yilun Zhu <zyl@meta.com>
2025-10-09 22:30:22 -0700

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Yilun Zhu on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
